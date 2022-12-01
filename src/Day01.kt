85f4c066cb61ee271e53ca7dc9483c07bc161af5
Christina Kohl <christina.kohl@uibk.ac.at>
2022-12-01 21:26:39 +0100

    Day 01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,35 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+
+    fun part1(input: List<List<Int>>): Int {
+        val sums = IntArray(input.size)
+        // sum up all sublists
+        input.withIndex().forEach {
+            sums[it.index] = it.value.sum()
+        }
+        return sums.max()
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: List<List<Int>>): Int {
+        val sums = IntArray(input.size)
+        // sum up all sublists
+        input.withIndex().forEach {
+            sums[it.index] = it.value.sum()
+        }
+        var top3calories = 0
+        for (i in 0 until 3) {
+            val maxIdx = sums.indices.maxBy { sums[it] }
+            top3calories += sums[maxIdx]
+            sums[maxIdx] = 0
+        }
+        return top3calories
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    //val testInput = readInputChunks("Day01_test")
 
-    val input = readInput("Day01")
-    println(part1(input))
+    //check(part2(testInput) == 45000)
+
+    val input = readInputChunks("Day01")
+    //println(part1(input))
     println(part2(input))
 }
--- src/Utils.kt
+++ src/Utils.kt
@@ -1,12 +1,27 @@
+import java.io.BufferedReader
 import java.io.File
+import java.io.FileReader
 import java.math.BigInteger
 import java.security.MessageDigest
 
 /**
  * Reads lines from the given input txt file.
  */
-fun readInput(name: String) = File("src", "$name.txt")
-    .readLines()
+fun readInputChunks(name: String): List<List<Int>> {
+    val file = File("src/input", "$name.txt")
+    val result = mutableListOf(mutableListOf<Int>())
+    BufferedReader(FileReader(file)).use { br ->
+        for (line in br.lines()) {
+            if (line.isEmpty()) {
+                result.add(mutableListOf())
+            } else {
+                result.last().add(line.toInt())
+            }
+        }
+    }
+    return result
+}
+
 
 /**
  * Converts string to md5 hash.
