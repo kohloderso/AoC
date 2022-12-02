f323b71f02585527c16b37b64aefdc055cb1c712
Christina Kohl <christina.kohl@uibk.ac.at>
2022-12-02 18:34:16 +0100

    Day 02

---
+++ src/Day02.kt
@@ -0,0 +1,49 @@
+fun main() {
+
+    fun scoreRound(opponent: Int, self: Int): Int {
+        val score =
+            when (self) {
+                opponent -> 3
+                opponent % 3 + 1 -> 6
+                else -> 0
+            }
+        return (score + self)
+    }
+
+    fun determineRPC(opponent: Int, outcome: Int) =
+        when (outcome) {
+            2 -> opponent
+            3 -> opponent % 3 + 1
+            else -> if(opponent >= 2) opponent - 1 else 3
+        }
+
+    fun part1(input: List<Pair<Int, Int>>): Int {
+        return input.fold(0) {sum, round -> sum + scoreRound(round.first, round.second)}
+    }
+
+    fun part2(input: List<Pair<Int, Int>>): Int {
+        return input.fold(0) {sum, round ->
+            val self = determineRPC(round.first, round.second)
+            sum + scoreRound(round.first, self)}
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = parseRPC("Day02_test")
+
+
+    check(scoreRound(2,1) == 1)
+    check(scoreRound(1,2) == 8)
+    check(scoreRound(1, 3) == 3)
+    check(scoreRound(2, 3) == 9)
+    check(scoreRound(3, 2) == 2)
+    check(scoreRound(3, 1) == 7)
+    check(scoreRound(1, 1) == 4)
+    check(scoreRound(2, 2) == 5)
+    check(scoreRound(3, 3) == 6)
+
+    check(part2(testInput) == 12)
+
+    val input = parseRPC("Day02")
+    //println(part1(input))
+    println(part2(input))
+}
--- src/Utils.kt
+++ src/Utils.kt
@@ -22,6 +22,29 @@ fun readInputChunks(name: String): List<List<Int>> {
     return result
 }
 
+fun parseRPC(name: String): List<Pair<Int, Int>> {
+    val lines = File("src/input", "$name.txt").readLines()
+    val result = lines.map{ line ->
+        val vals = line.split(' ')
+        val opponent =
+            when(vals[0]) {
+                "A" -> 1
+                "B" -> 2
+                "C" -> 3
+                else -> -1
+            }
+        val self =
+            when(vals[1]) {
+                "X" -> 1
+                "Y" -> 2
+                "Z" -> 3
+                else -> -1
+            }
+
+        Pair(opponent, self)
+    }
+    return result
+}
 
 /**
  * Converts string to md5 hash.
