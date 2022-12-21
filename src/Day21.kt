ae0377cb03a71117c33efb832fa9a7513be1125b
Christina Kohl <christina.kohl@uibk.ac.at>
2022-12-21 20:44:18 +0100

    Day 21 part 2

--- src/Day21.kt
+++ src/Day21.kt
@@ -46,17 +46,92 @@ fun main() {
         return result
     }
 
+    data class Operation(val eval: (Long, Long) -> Long, val solveLeft: (Long, Long) -> Long, val solveRight: (Long, Long) -> Long)
+
+    val plusOp = Operation({x,y -> x+y}, {x,y -> x-y}, {x,y -> x-y})
+    val minusOp = Operation({x,y -> x-y}, {x,y -> x+y}, {x,y -> y-x})
+    val timesOp = Operation({x,y -> x*y}, {x,y -> x/y}, {x,y -> x/y})
+    val divOp = Operation({x,y -> x/y}, {x,y -> x*y}, {x,y -> y/x})
+
+    class MonkeyOp() {
+        var value: Long? = null
+        var eval: (Map<String, MonkeyOp>) -> Long? = { null }
+        var solve: (Map<String, MonkeyOp>, Long) -> Long? = {_,x -> x }
+
+        constructor(value: Long) : this() {
+            this.value = value
+            this.eval = { value }
+            this.solve = { _,x -> if(x == value) x else null }
+        }
+        constructor(left: String, right: String, op: Operation) : this() {
+            eval = { map ->
+                if(value != null) value
+                else {
+                    val op1 = map[left]?.eval?.invoke(map)
+                    val op2 = map[right]?.eval?.invoke(map)
+                    if (op1 != null && op2 != null) {
+                        value = op.eval(op1, op2)
+                        value
+                    } else null
+                }
+            }
+            solve = { map, x ->
+                val op1 = map[left]?.eval?.invoke(map)
+                val op2 = map[right]?.eval?.invoke(map)
+                if(op1 == null && op2 != null) {
+                    map[left]?.solve?.invoke(map, op.solveLeft(x, op2))
+                } else if(op1 != null && op2 == null) {
+                    map[right]?.solve?.invoke(map, op.solveRight(x, op1))
+                } else null
+            }
+        }
+    }
+
+    fun part2(input: List<String>): Long? {
+        val monkeyMap = mutableMapOf<String,MonkeyOp>()
+        var left: String = ""
+        var right: String = ""
+        for(line in input) {
+            val split = line.split(":")
+            val name = split[0]
+            val yell = split[1].trim()
+            if(yell.toLongOrNull() != null) {
+                monkeyMap[name] =
+                    if(name == "humn") MonkeyOp()
+                    else MonkeyOp(yell.toLong())
+            } else {
+                val op = yell.split(" ")
+                if(name == "root") {
+                    left = op[0]
+                    right = op[2]
+                }
+                when(op[1]) {
+                    "+" -> monkeyMap[name] = MonkeyOp(op[0], op[2], plusOp)
+                    "-" -> monkeyMap[name] = MonkeyOp(op[0], op[2], minusOp)
+                    "/" -> monkeyMap[name] = MonkeyOp(op[0], op[2], divOp)
+                    "*" -> monkeyMap[name] = MonkeyOp(op[0], op[2], timesOp)
+                }
+            }
+        }
+        val leftRes = monkeyMap[left]?.eval?.invoke(monkeyMap)
+        val rightRes = monkeyMap[right]?.eval?.invoke(monkeyMap)
+        return if(leftRes != null) {
+            monkeyMap[right]?.solve?.invoke(monkeyMap, leftRes)
+        } else if (rightRes != null) {
+            monkeyMap[left]?.solve?.invoke(monkeyMap, rightRes)
+        } else null
+    }
+
     val test = parseLines("Day21_test")
 
-    val start = System.currentTimeMillis()
+
 //    check(part1(test) == 152L)
-//    check(part2(test) == 1623178306L)
-//
-//
+    check(part2(test) == 301L)
+
     val input = parseLines("Day21")
-////    val blueprints = parseBlueprints(input)
-    println(part1(input))
-//    println(part2(input))
-////
+//    println(part1(input))
+    val start = System.currentTimeMillis()
+    println(part2(input))
+
    println("time: " + (System.currentTimeMillis() - start))
 }
